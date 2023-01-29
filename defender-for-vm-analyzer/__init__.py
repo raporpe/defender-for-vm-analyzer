c1ec5a709d081892ab10a8e625d7d9f5ad6ee4b1
Raúl P <raporpe@gmail.com>
2023-01-29 13:57:00 +0100

    improve the code and fix bugs

--- defender-for-vm-analyzer/__init__.py
+++ defender-for-vm-analyzer/__init__.py
@@ -3,11 +3,16 @@ import logging
 logger = logging.getLogger(__name__)
 import os
 
+from . import credential_adapter
+
 import azure.functions as func
 
 # import the Azure SDK for Python management
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.compute import ComputeManagementClient
+
+from azure.mgmt.resource import ResourceManagementClient
+
 
 def main(mytimer: func.TimerRequest) -> None:
     utc_timestamp = datetime.datetime.utcnow().replace(
@@ -24,19 +29,40 @@ def main(mytimer: func.TimerRequest) -> None:
     running_vms = get_running_vms(subscription_id)
 
     # Send the number of running Azure VMs to App Insights 
-    logger.error('Running VMs: %s', running_vms, extra={'custom_dimensions': {'running': running_vms}})
+    logger.info('Running VMs: %s', running_vms, extra={'custom_dimensions': {'running': running_vms}})
 
 
 def get_running_vms(subscription_id):
     # Get the number of running Azure VMs in susbscription id
-    credential = DefaultAzureCredential()
-    compute_client = ComputeManagementClient(credential, subscription_id)
+    credentials = DefaultAzureCredential()
+
+    # Use adapter to avoid the following error: 'DefaultAzureCredential' object has no attribute 'signed_session'
+    wrapped_credential = credential_adapter.AzureIdentityCredentialAdapter(credentials)
+
+
+    compute_client = ComputeManagementClient(wrapped_credential,
+                                             subscription_id)
     running_vms = 0
+
+    vm : azure.mgmt.compute.v2019_07_01.models._models_py3.VirtualMachine = None
+
     for vm in compute_client.virtual_machines.list_all():
         vm_resource_group = vm.id.split('/')[4]
-        status = compute_client.virtual_machines.instance_view(vm_resource_group, vm.name)
-        logger.info("--------------- VM PLAN ----------> " + str(vm.plan))
-        if status.statuses[1].display_status == "VM running" and vm.plan == "DatabricksWorker":
+        logger.warn(vm.name)
+
+        # Get the running status of the VM
+        vm_status = compute_client.virtual_machines.instance_view(resource_group_name=vm_resource_group,
+                                                                  vm_name=vm.name)
+
+        logger.warn(vm_status.statuses[1].display_status)
+
+        billable_databricks_vm = vm.provisioning_state == "Succeeded" and \
+            vm.plan == "DatabricksWorker" and \
+            vm_status.statuses[1].display_status == "VM running"
+
+        if billable_databricks_vm:
             running_vms += 1
+            logger.info("The VM {} is a billable databricks VM".format(vm.name))
+
     return running_vms
     
